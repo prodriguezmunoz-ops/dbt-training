54519c761c7e23c12c0486e77ec343fb58dae40a
Patri Rodri <p.rodriguezmunoz@gmail.com>
2026-09-08 23:58:23 +0000

    working with yml

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -6,10 +6,12 @@ select
     o.shipmode,
     o.ordersellingprice - o.ordercostprice as orderprofit,
     --from raw_customer
+    c.customerid,
     c.customername,
     c.segment,
     c.country,
     --from raw_product
+    p.productid,
     p.category,
     p.productname,
     p.subcategory
